06f2b868a11245b5cb438b4bde06ce25c462d84b
Atul Maurya <57825554+atulmaurya112@users.noreply.github.com>
2026-06-06 18:17:21 +0530

    Fix syntax for withCredentials in push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,9 +1,9 @@
 def call() {
     withCredentials([usernamePassword(
-            credentialsId: 'dockerHubCredential',
-            usernameVariable: 'DOCKER_USERNAME',
-            passwordVariable: 'DOCKER_PASSWORD'
-        )]) {
+        credentialsId: 'dockerHubCredential',
+        usernameVariable: 'DOCKER_USERNAME',
+        passwordVariable: 'DOCKER_PASSWORD'
+    )]) {
         sh '''
         echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
         docker tag web-app:latest atulmaurya112/web-app:latest

271a8918336866c29621e5837a3e1ce9f65e8331
Atul Maurya <57825554+atulmaurya112@users.noreply.github.com>
2026-06-06 18:38:44 +0530

    Enhance clone function with additional git commands
    Added commands to list directory contents and show git branches.

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,11 @@
 def call() {
-  git url:'https://github.com/atulmaurya112/test-web-app', branch: 'main'
-  sh '''pwd'''
+    git url: 'https://github.com/atulmaurya112/test-web-app',
+        branch: 'main'
+
+    sh '''
+        pwd
+        ls -la
+        git branch
+        git remote -v
+    '''
 }
